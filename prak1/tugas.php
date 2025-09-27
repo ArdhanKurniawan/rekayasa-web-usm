78d433dd7e4c8a591bf1b37b633c17327a894cf6
unknown <ardhank08@gmail.com>
2025-09-27 18:59:04 +0700

    tugas praktikum 1

---
+++ prak1/tugas.php
@@ -0,0 +1,25 @@
+<?php 
+    $backend_languange = [
+        "a" => "PHP",
+        "b" => "Python",
+        "c" => "Java"
+    ];
+    
+    $backend_languange_json = json_encode($backend_languange);
+    echo $backend_languange_json;
+    echo "<br>";
+
+
+
+    $decode_object = json_decode($backend_languange_json);
+    $decode_array = json_decode($backend_languange_json, true);
+
+
+    var_dump($decode_object);
+    echo "<br>";
+    echo $decode_object->a;
+    echo "<br>";
+
+    var_dump($decode_array);
+    echo "<br>";
+    echo $decode_array['a'];
